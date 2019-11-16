bc8fff46ed7bbb1964c1623abc60d44dbf113986
Mario <marioquiro17@gmail.com>
2019-11-16 15:32:06 -0300

    Init data access layer files

--- src/main/java/jpa/entity/User.java
+++ src/main/java/jpa/entity/User.java
@@ -22,11 +22,11 @@ public class User {
     private Set<Post> posts = new HashSet<>();
 
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
